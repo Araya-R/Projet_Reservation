f1ead2c24f445cacba8fd6d9e3f5e8f90b4c5124
Araya RAKSACHOB <araya.raksachob@lapiscine.pro>
2025-04-16 10:50:02 +0200

    cancelPages

--- controller/cancel-reservation-controller.php
+++ controller/cancel-reservation-controller.php
@@ -8,4 +8,28 @@ require_once('../model/reservation-repository.php');
 //celle ci est stockée ensuite dans la variable $reservationForUser
 $reservationForUser= findReservationForUser();
 
+
+//je vérifie si le formulaire a bien été envoyé
+if($_SERVER["REQUEST_METHOD"]=== "POST"){
+    
+    //je récupère la réservation 
+    $reservationForUser= findReservationForUser();
+
+    if($reservationForUser){
+
+        //annuler la réservation
+        //la fonction cancel () existe déjà dans reservation-model
+        $reservationForUser->cancel();
+
+        //réenregistrer dans la session
+        //la fonction persistReservation existe dans reservation-repository
+        persistReservation($reservationForUser);
+
+        //afficher un message 
+        echo "La réservation a bien été annulée.";
+    }else{
+        echo "Aucune réservation à annuler.";
+    }
+}
+
 require_once('../view/cancel-reservation-view.php');
--- model/reservation-repository.php
+++ model/reservation-repository.php
@@ -1,17 +1,25 @@
 <?php
 
+//Pour évviter les appels multiples à session_start():
+//session_start() ne peut être appelé qu'une seule fois par requête HTTP.
+function startSessionIfNotStarted() {
+    if (session_status() == PHP_SESSION_NONE) {
+        session_start();
+    }
+}
+
 //Utilisation de la session = stocker temporairement une réservation
 function persistReservation ($Reservation){
 
     //je démarre la session et j'enregistre l'objet Reservation dans la session
-    session_start();
+    startSessionIfNotStarted();
     $_SESSION["Reservation"] = $Reservation;
 }
 
 function findReservationForUser(){
     
     //on stock les données grâce à la session
-    session_start();
+    startSessionIfNotStarted();
 
     //on vérifie si la clé Reservation existe dans $_SESSION
     if(array_key_exists('Reservation',$_SESSION)){
